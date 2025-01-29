797755b2e647fba46e1a86a984fa4bedd392e510
Shane Rosenthal <srosenthal82@gmail.com>
2025-01-29 08:15:00 -0500

    u

--- app/Filament/Resources/DeveloperResource.php
+++ app/Filament/Resources/DeveloperResource.php
@@ -15,6 +15,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -164,7 +165,10 @@ class DeveloperResource extends Resource
 
             ])
             ->filters([
-                //
+                TernaryFilter::make('is_reviewed')
+                    ->nullable(),
+                TernaryFilter::make('is_selected')
+                    ->nullable()
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
